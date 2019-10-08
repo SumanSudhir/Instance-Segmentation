4f6cb26849443c8d84a3b21158d930856a704dab
SumanSudhir <sudhiriitb27@gmail.com>
2019-10-09 01:18:28 +0530

    cocodataset tested

--- cocodataset.py
+++ cocodataset.py
@@ -11,8 +11,7 @@ import cv2
 from utils import get_hierarchy, find_contour
 
 def get_args():
-    parser = argparse.ArgumentParser(description="This script creates coco format dataset for maskrcnn-benchmark",
-                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description="This script creates coco format dataset for maskrcnn-benchmark", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("--layer", "-l", type=int,default=0,
                                 help="target layer; 0 or 1")
@@ -25,6 +24,17 @@ def get_args():
 
     args = parser.parse_args()
     return args
+
+def calc_overlap_rate(rect1, rect2):
+    x_left = max(rect1.x1, rect2.x1)
+    x_right = min(rect1.x2, rect1.x2)
+    y_top = max(rect1.y1, rect2.y1)
+    y_bottom = min(rect1.y2, rect2.y2)
+
+    intersection = max(0, x_right - x_left) * max(0, y_bottom - y_top)
+    iou = intersection/rect1.area
+
+    return iou
 
 class Rect:
     def __init__(self,x1,y1,x2,y2):
@@ -40,17 +50,6 @@ class Rect:
         return self.x1 <= x <= self.x2 and self.y1 <= y <= self.y2
 
 
-def calc_overlap_rate(rect1, rect2):
-    x_left = max(rect1.x1, rect2.x1)
-    x_right = min(rect1.x2, rect1.x2)
-    y_top = max(rect1.y1, rect2.y1)
-    y_bottom = min(rect1.y1, rect2.y1)
-
-    intersection = max(0,x_right - x_left)*max(0, y_bottom-y_top)
-    iou = intersection/rect1.area
-
-    return iou
-
 def main():
     args = get_args()
     layer = args.layer
@@ -61,16 +60,14 @@ def main():
     layer0_class_strings, layer1_class_strings, class_string_to_parent = get_hierarchy()
     target_class_strings = get_hierarchy()[layer]
 
-    target_class_string_to_class_id = {class_string:i+1 for i, class_string in
-                                        enumerate(sorted(target_class_strings))}
+    target_class_string_to_class_id = {class_string:i+1 for i, class_string in enumerate(sorted(target_class_strings))}
 
-    parent_class_string = list(set(class_string_to_parent.values()))
-    layer0_independent_class_strings = [class_string for class_string in layer0_class_strings if
-                                                        class_string not in parent_class_string]
+    parent_class_strings = list(set(class_string_to_parent.values()))
+    layer0_independent_class_strings = [class_string for class_string in layer0_class_strings if class_string not in parent_class_strings]
 
-    data_dir = Path("__file__").parent.joinpath('datasets')
+    data_dir = Path(__file__).parent.joinpath('datasets')
     img_dir = data_dir.joinpath(f"{mode}")
-    mask_dir = data_dir.joinpath(f"{mode}_mask")
+    mask_dir = data_dir.joinpath(f"{mode}_masks")
     mask_csv_path = data_dir.joinpath(f"challenge-2019-{mode}-segmentation-masks.csv")
     df = pd.read_csv(str(mask_csv_path))
 
@@ -78,7 +75,7 @@ def main():
     output_dir.mkdir(parents=True, exist_ok=True)
     output_annotation_dir = output_dir.joinpath("annotations")
     output_annotation_dir.mkdir(parents=True, exist_ok=True)
-    output_image_dir = output_dir.joinpath("train2017")
+    output_img_dir = output_dir.joinpath("train2017")
     output_img_dir.mkdir(parents=True, exist_ok=True)
 
     class_string_to_img_ids = defaultdict(list)
@@ -92,17 +89,15 @@ def main():
         img_id_to_meta[img_id].append({"mask_path": mask_path, "class_string": label_name, "bbox": [xp1,xp2,yp1,yp2]})
 
     # use only args.img_num of images of each class
-    target_img_ids = list(
-        set(chain.from_iterable(
-            [class_string_to_img_ids[class_string][:args.img_num] for class_string in target_class_strings])))
+    target_img_ids = list(set(chain.from_iterable([class_string_to_img_ids[class_string][:args.img_num] for class_string in target_class_strings])))
+
     print("=> use {} images for training".format(len(target_img_ids)))
     bbox_id = 0
 
     for i, img_id in enumerate(tqdm(target_img_ids)):
         added = False
         img_path = img_dir.joinpath(img_id + ".jpg")
-        #print(img_path)
-        img = cv2.imread(str(img_path),1)
+        img = cv2.imread(str(img_path), 1)
         h, w, _ = img.shape
         target_rects = []
 
@@ -120,7 +115,7 @@ def main():
         for m in img_id_to_meta[img_id]:
             class_string = m["class_string"]
             xp1, xp2, yp1, yp2 = m["bbox"]
-            x1, x2, y1, y2 = xp1*w , xp2*w, yp1*w, yp2*w
+            x1, x2, y1, y2 = xp1*w , xp2*w, yp1*h, yp2*h
 
             # For layer1: remove layer0 with no child class
             if layer == 1 and class_string in layer0_independent_class_strings:
@@ -184,11 +179,10 @@ def main():
                         "width": w,
                         "id": i})
 
-        output_img_path = output_image_dir.joinpath(img_path.name)
+        output_img_path = output_img_dir.joinpath(img_path.name)
         cv2.imwrite(str(output_img_path),img)
 
-    categories = [{"supercategory": "object", "id": class_id, "name": class_string} for class_string, class_id in
-                  target_class_string_to_class_id.items()]
+    categories = [{"supercategory": "object", "id": class_id, "name": class_string} for class_string, class_id in target_class_string_to_class_id.items()]
 
     with output_annotation_dir.joinpath("instances_train2017.json").open("w") as f:
         json.dump({"images":images, "annotations":annotations, "categories": categories}, f)
