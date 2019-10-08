dbfc39067f4bbc8f53010b1a25c81fc328224518
SumanSudhir <sudhiriitb27@gmail.com>
2019-10-08 19:39:40 +0530

    cocodataset code done

--- cocodataset.py
+++ cocodataset.py
@@ -68,7 +68,7 @@ def main():
     layer0_independent_class_strings = [class_string for class_string in layer0_class_strings if
                                                         class_string not in parent_class_string]
 
-    data_dir = Path("__file__").parent,joinpath('datasets')
+    data_dir = Path("__file__").parent.joinpath('datasets')
     img_dir = data_dir.joinpath(f"{mode}")
     mask_dir = data_dir.joinpath(f"{mode}_mask")
     mask_csv_path = data_dir.joinpath(f"challenge-2019-{mode}-segmentation-masks.csv")
@@ -79,7 +79,7 @@ def main():
     output_annotation_dir = output_dir.joinpath("annotations")
     output_annotation_dir.mkdir(parents=True, exist_ok=True)
     output_image_dir = output_dir.joinpath("train2017")
-    output_annotation_dir.mkdir(parents=True, exist_ok=True)
+    output_img_dir.mkdir(parents=True, exist_ok=True)
 
     class_string_to_img_ids = defaultdict(list)
     img_id_to_meta = defaultdict(list)
@@ -88,7 +88,7 @@ def main():
 
     for i, row in tqdm(df.iterrows(),total = len(df)):
         mask_path, img_id, label_name, _, xp1, xp2, yp1, yp2, _, _ = row.values
-        class_string_to_img_ids[label_name].append(im_id)
+        class_string_to_img_ids[label_name].append(img_id)
         img_id_to_meta[img_id].append({"mask_path": mask_path, "class_string": label_name, "bbox": [xp1,xp2,yp1,yp2]})
 
     # use only args.img_num of images of each class
@@ -101,6 +101,7 @@ def main():
     for i, img_id in enumerate(tqdm(target_img_ids)):
         added = False
         img_path = img_dir.joinpath(img_id + ".jpg")
+        #print(img_path)
         img = cv2.imread(str(img_path),1)
         h, w, _ = img.shape
         target_rects = []
@@ -129,3 +130,69 @@ def main():
             if class_string not in target_class_strings:
                 curr_rect = Rect(xp1, yp1, xp2, yp2)
                 overlap_rate = max([calc_overlap_rate(r,curr_rect) for r in target_rects])
+
+                if overlap_rate > 0.25:
+                    continue
+
+            #layer0: convert layer1 and layer2 classes to their layer0 classes
+            if layer == 0:
+                if class_string in class_string_to_parent.keys():
+                    class_string = class_string_to_parent[class_string]
+
+                #needed for layer2 classes
+                if class_string in class_string_to_parent.keys():
+                    class_string = class_string_to_parent[class_string]
+
+            if class_string in target_class_strings:
+                mask_path = mask_dir.joinpath(m["mask_path"])
+                mask_img = cv2.imread(str(mask_path),0)
+                mask_img = cv2.resize(mask_img,(w,h), cv2.INTER_NEAREST)
+                contour = find_contour(mask_img)
+                contour = [p for p in contour if len(p) > 4]
+
+                if not contour:
+                    continue
+
+                class_id = target_class_string_to_class_id[class_string]
+                gt_bbox = [x1, y1, x2-x1, y2-y1]
+                box_w, box_h = x2 - x1, y2 - y1
+
+                if box_w < 10 or box_h <10:
+                    print(box_w, box_h)
+
+                annotations.append({
+                                    "segmentation": contour,
+                                    "iscrowd": 0,
+                                    "image_id": i,
+                                    "bbox":gt_bbox,
+                                    "category_id": class_id,
+                                    "id":bbox_id})
+
+                bbox_id += 1
+                added = True
+
+            # for layer1: fill non-target bbox with gray; this class has its child in layer 1
+            else:
+                x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
+                img[y1:y2, x1:x2] = 128
+
+        if not added:
+            continue
+
+        images.append({"file_name":img_path.name,
+                        "height": h,
+                        "width": w,
+                        "id": i})
+
+        output_img_path = output_image_dir.joinpath(img_path.name)
+        cv2.imwrite(str(output_img_path),img)
+
+    categories = [{"supercategory": "object", "id": class_id, "name": class_string} for class_string, class_id in
+                  target_class_string_to_class_id.items()]
+
+    with output_annotation_dir.joinpath("instances_train2017.json").open("w") as f:
+        json.dump({"images":images, "annotations":annotations, "categories": categories}, f)
+
+
+if __name__ == "__main__":
+    main()
